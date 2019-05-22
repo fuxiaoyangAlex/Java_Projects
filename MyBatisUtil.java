59efc19500407ef7f2b370b3b906e4a04682c314
Erick <Wang.zhuang@outlook.com>
2019-05-22 09:03:18 +0800

    Update MyBatisUtil.java

--- MyBatisUtil.java
+++ MyBatisUtil.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
+ 
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
